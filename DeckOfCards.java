ef300738d534eea4070e6b7d55e4545aae5faf74
Sanjana Rao <sanjanagrao99@gmail.com>
2021-09-08 08:16:19 +0530

    Added: Display message and created a Maven Project

---
+++ DeckOfCards.java
@@ -0,0 +1,15 @@
+package com.WorkshopProblem;
+
+public class DeckOfCards 
+{
+	public DeckOfCards()
+	{
+		System.out.println("Welcome to the Card Game.");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
