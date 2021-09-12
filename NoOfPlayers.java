234d2f1c52d2b5fc5c1632b7751a14a88a7b8cf6
Sanjana Rao <sanjanagrao99@gmail.com>
2021-09-12 13:25:27 +0530

    Added: Method added to get the sequence of players order to play and get cards

--- NoOfPlayers.java
+++ NoOfPlayers.java
@@ -1,18 +1,42 @@
 package com.WorkshopProblem;
 
 import java.util.ArrayList;
+import java.util.Collections;
 /*
 *  The class NoOfPlayers implements the class to get the number of players and initialize cards to them.
-*  An object playerCards of class DeckOfCards is created
-*  An ArrayList is created of interface distributeCards to distribute class to the number of players.
+*  @param noOfPlayers tells the number of players playing the card game.
 *  @author Sanjana Rao
 *  @since 11-09-2021
 */
 public class NoOfPlayers 
 { 
+	private int noOfPlayers;
+	
+	/*
+	 * Constructor is given to initialize the @param noOfPlayers
+	 * An object playerCards of class DeckOfCards is created.
+	 *  An ArrayList is created of interface distributeCards to distribute class to the number of players.
+	 */
 	public NoOfPlayers(int noOfPlayers)
 	    {
-	        DeckOfCards playerCards = new DeckOfCards();
+	        this.noOfPlayers = noOfPlayers;
+			DeckOfCards playerCards = new DeckOfCards();
 	        ArrayList<distributeCards> numberOfPlayers = new ArrayList<distributeCards>(noOfPlayers);    
 	    }
+	
+	/*
+	 * The method PlayerOrder is the method to sequence the Players order and how they should receive the cards and further order for play.
+	 * @param sequence is the ArrayList of integer datatype to sequence players' order.
+	 * for loop is used to add the order.
+	 * In built method shuffle is used to randomly permute the specified list using a default source of randomness.
+	 */
+	public void PlayerOrder()
+	{
+		ArrayList<Integer> sequence = new ArrayList<Integer>(noOfPlayers);
+	    for(int order=0; order<noOfPlayers; order++)
+	    {
+	    	sequence.add(order);
+	    }
+	    Collections.shuffle(sequence);
+	}
 }
--- distributeCards.java
+++ distributeCards.java
@@ -2,7 +2,7 @@ package com.WorkshopProblem;
 
 import java.util.ArrayList;
 /*
-*  An interface distributeCards is created to to store the number of players and cards they have.
+*  An interface distributeCards is created to initialize card for the players.
 *  An ArrayList is created of with size 9 because each player needs to have 9 cards each.
 *  @author Sanjana Rao
 *  @since 11-09-2021
